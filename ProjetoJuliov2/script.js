3dd94d00c38611dc30b558a1edb0c2776542cb79
mmToscano <107902881+mmToscano@users.noreply.github.com>
2022-11-06 17:29:35 -0300

    Add files via upload

--- ProjetoJuliov2/script.js
+++ ProjetoJuliov2/script.js
@@ -92,3 +92,27 @@ function organizar(){
 
 
 }
+
+
+//O código abaixo trata-se da manipulação do gráfico do Chart.js
+
+colorPatters = ['red', 'purple', 'yellow'];
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+    type: 'bar',
+
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: colorPatters,
+            borderColor: colorPatters,
+            borderWidth: 5,
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    options: {}
+});
